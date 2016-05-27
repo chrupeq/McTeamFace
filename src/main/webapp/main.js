5339a1f4945ed2670555d01c31613cf8a5c72121
laurahunt <laurajanehunt28@gmail.com>
2016-05-27 12:37:07 +0100

    Project login page css design created for html template.

---
+++ src/main/webapp/main.js
@@ -0,0 +1,18 @@
+//javascript document
+
+
+window.onload = prepareLogin;
+
+function prepareLogin() {
+	document.getElementById("button").onclick = signin;
+}
+
+var signin = function() {
+	var username = document.getElementById("username").value;
+	var password = document.getElementById("password").value;
+	
+	if(username == "Manager2016" && password == "password1") {
+		alert("Login SUCCESS");
+	}
+
+}
