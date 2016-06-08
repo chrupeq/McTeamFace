1454592e148e11b4d5e11675318fe800f770729c
laurahunt <laurajanehunt28@gmail.com>
2016-06-08 18:05:54 +0100

    Navigation/Form/Bootstrap
    Navigation between different content is now working using tabs. Form
    html and css completed.

--- src/main/webapp/resources/home.js
+++ src/main/webapp/resources/home.js
@@ -1,40 +1,11 @@
 
 
-//var rootUrl="http://localhost:8080/GroupProject2016/rest/users";
+var rootUrl="http://localhost:8080/GroupProject2016/rest/users";
 
 $(document).ready(function() {
 
 });
 
 
-$(document).on("click", "homeTab", function(){
-	alert("Home");
-	window.location = 'http://localhost:8080/GroupProject2016/home.html';
-	
-	
-});
 
-$(document).on("click", "dataSet", function(){
-	alert("Data");
-	window.location = 'http://localhost:8080/GroupProject2016/dataset.html';
-	
-	
-});
-
-
-
-$(document).on("click", "aboutUs", function(){
-	alert("About");
-	window.location = 'http://localhost:8080/GroupProject2016/aboutUs.html';
-	
-
-});
-
-
-$(document).on("click", "contactUs", function(){
-	alert("Contact");
-	window.location = 'http://localhost:8080/GroupProject2016/contactUs.html';
-	
-
-});
 
