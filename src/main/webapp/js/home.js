3ce1129e5820721176e0f3663d651f616b8f0e3d
laurahunt <laurajanehunt28@gmail.com>
2016-06-08 13:42:41 +0100

    Jquery/JavaScript/Index page after login/BootStrap tabs and html pages for each.
    Index page for after login added. Bootstrap tabs created. Pages for each
    tab created also. Javascript/jquery on click events coded.

---
+++ src/main/webapp/js/home.js
@@ -0,0 +1,37 @@
+
+
+var rootUrl="http://localhost:8080/GroupProject2016/rest/users";
+
+
+$(document).on("click", "homeTab", function(){
+	window.location = 'http://localhost:8080/GroupProject2016/home.html';
+	
+	return false;
+});
+
+$(document).on("click", "dataSet", function(){
+	window.location = 'http://localhost:8080/GroupProject2016/dataset.html';
+	
+	return false;
+});
+
+$(document).on("click", "form", function(){
+	window.location = 'http://localhost:8080/GroupProject2016/form.html';
+	
+	return false;
+});
+
+
+$(document).on("click", "aboutUs", function(){
+	window.location = 'http://localhost:8080/GroupProject2016/aboutUs.html';
+	
+	return false;
+});
+
+
+$(document).on("click", "contactUs", function(){
+	window.location = 'http://localhost:8080/GroupProject2016/contactUs.html';
+	
+	return false;
+});
+
