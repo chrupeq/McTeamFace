939fd4be4f7f2547bdf3ee7732117f8f37b2d123
chrupeq <a00160506@student.ait.ie>
2016-06-09 19:03:07 +0100

    Webservice to database life cycle tested for all entities

---
+++ src/test/java/com/dao/test/Failure_classClientWebServiceLifecycleTest.java
@@ -0,0 +1,140 @@
+package com.dao.test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.net.URL;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.MediaType;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.junit.InSequence;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.resteasy.client.ClientRequest;
+import org.jboss.resteasy.client.ClientResponse;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.ait.db.data.NetworkEntityDAO;
+import com.ait.db.model.Base_data;
+import com.ait.db.model.Failure_class;
+import com.ait.db.rest.JaxRsActivator;
+import com.ait.db.rest.NetworkEntityRestService;
+
+@RunWith(Arquillian.class)
+@RunAsClient
+public class Failure_classClientWebServiceLifecycleTest {
+	private static final String RESOURCE_PREFIX = JaxRsActivator.class.getAnnotation(ApplicationPath.class).value().substring(1);
+	private ClientRequest request;
+	private ClientResponse<String> responseObj;
+	private String jsonData;
+
+	@Deployment(testable = false)
+    public static WebArchive createDeployment() {
+        return ShrinkWrap.create(WebArchive.class, "test.war").addPackage(Failure_class.class.getPackage())
+        		.addPackage(NetworkEntityDAO.class.getPackage())
+        		.addClasses(NetworkEntityRestService.class, JaxRsActivator.class)
+        		.addAsResource("test-persistence.xml", "META-INF/persistence.xml")
+                .addAsResource("import.sql")
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+	}
+	@ArquillianResource
+    URL deploymentUrl;
+	
+	@Test 
+	@InSequence(1)
+	public void getAllFailure_classEntities() throws Exception {
+		request = new ClientRequest(deploymentUrl.toString() + RESOURCE_PREFIX + "/network_entities/failure_class");
+		request.header("Accept", MediaType.APPLICATION_JSON);
+		// we're expecting a String back
+        ClientResponse<String> responseObj = request.get(String.class);
+        assertEquals(200, responseObj.getStatus());
+        String response = responseObj.getEntity().trim();
+        System.out.println("The response is: " + response);
+	}
+	
+	@Test
+	@InSequence(2)
+	public void testFailure_classNotFoundById() throws Exception {
+		request = new ClientRequest(deploymentUrl.toString() + RESOURCE_PREFIX + "/network_entities/failure_class/1");
+		request.header("Accept", MediaType.APPLICATION_JSON);
+		// we're expecting a String back
+        responseObj = request.get(String.class);
+        assertEquals(404, responseObj.getStatus());
+        String response = responseObj.getEntity().trim();
+        System.out.println("The response is: " + response);
+	}
+	
+	@Test
+	@InSequence(3)
+	public void testPostFailure_class() throws Exception {
+		jsonData = "{\"failure_class\":\"1\", \"description\":\"EMERGENCY\"}";
+		request = new ClientRequest(deploymentUrl.toString() + RESOURCE_PREFIX + "/network_entities/failure_class");
+		request.accept("application/json").body(MediaType.APPLICATION_JSON, jsonData);
+		responseObj = request.post(String.class);
+		assertEquals(201, responseObj.getStatus());
+		String response = responseObj.getEntity().trim();
+		System.out.println("The response is: " + response);
+		
+	}
+	
+	@Test
+	@InSequence(4)
+	public void testCreatedFailure_classFoundById() throws Exception {
+		request = new ClientRequest(deploymentUrl.toString() + RESOURCE_PREFIX + "/network_entities/failure_class/1");
+		request.header("Accept", MediaType.APPLICATION_JSON);
+		// we're expecting a String back
+        responseObj = request.get(String.class);
+        assertEquals(200, responseObj.getStatus());
+        String response = responseObj.getEntity().trim();
+        System.out.println("The response is: " + response);
+	}
+	
+	@Test
+	@InSequence(5)
+	public void testPutFailure_class() throws Exception {
+		jsonData = "{\"failure_class\":\"2\", \"description\":\"EMERGENCY\"}";
+		request = new ClientRequest(deploymentUrl.toString() + RESOURCE_PREFIX + "/network_entities/failure_class/1");
+		request.accept("application/json").body(MediaType.APPLICATION_JSON, jsonData);
+		responseObj = request.put(String.class);
+		assertEquals(200, responseObj.getStatus());
+		String response = responseObj.getEntity().trim();
+		System.out.println("The response is: " + response);
+		
+	}
+	
+	@Test
+	@InSequence(6)
+	public void testChangedFailure_classFoundById() throws Exception {
+		request = new ClientRequest(deploymentUrl.toString() + RESOURCE_PREFIX + "/network_entities/failure_class/2");
+		request.header("Accept", MediaType.APPLICATION_JSON);
+		// we're expecting a String back
+        responseObj = request.get(String.class);
+        assertEquals(200, responseObj.getStatus());
+        String response = responseObj.getEntity().trim();
+        System.out.println("The response is: " + response);
+	}
+	@Test
+	@InSequence(7)
+	public void testFailure_classDeleteById() throws Exception {
+		request = new ClientRequest(deploymentUrl.toString() + RESOURCE_PREFIX + "/network_entities/failure_class/2");
+		request.header("Accept", MediaType.APPLICATION_JSON);
+		// we're expecting a String back
+        responseObj = request.delete(String.class);
+        assertEquals(204, responseObj.getStatus());
+	}
+	@Test
+	@InSequence(8)
+	public void testFailure_classNotFoundAfterDeleteById() throws Exception {
+		request = new ClientRequest(deploymentUrl.toString() + RESOURCE_PREFIX + "/network_entities/failure_class/3");
+		request.header("Accept", MediaType.APPLICATION_JSON);
+		// we're expecting a String back
+        responseObj = request.get(String.class);
+        assertEquals(404, responseObj.getStatus());
+	}
+}
---
+++ src/test/java/com/dao/test/Mcc_mncClientWebServiceLifecycleTest.java
@@ -0,0 +1,141 @@
+package com.dao.test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.net.URL;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.MediaType;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.junit.InSequence;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.resteasy.client.ClientRequest;
+import org.jboss.resteasy.client.ClientResponse;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.ait.db.data.NetworkEntityDAO;
+import com.ait.db.model.Event_cause;
+import com.ait.db.model.Mcc_mnc;
+import com.ait.db.rest.JaxRsActivator;
+import com.ait.db.rest.NetworkEntityRestService;
+
+@RunWith(Arquillian.class)
+@RunAsClient
+public class Mcc_mncClientWebServiceLifecycleTest {
+	private static final String RESOURCE_PREFIX = JaxRsActivator.class.getAnnotation(ApplicationPath.class).value().substring(1);
+	private ClientRequest request;
+	private ClientResponse<String> responseObj;
+	private String jsonData;
+
+	@Deployment(testable = false)
+    public static WebArchive createDeployment() {
+        return ShrinkWrap.create(WebArchive.class, "test.war").addPackage(Mcc_mnc.class.getPackage())
+        		.addPackage(NetworkEntityDAO.class.getPackage())
+        		.addClasses(NetworkEntityRestService.class, JaxRsActivator.class)
+        		.addAsResource("test-persistence.xml", "META-INF/persistence.xml")
+                .addAsResource("import.sql")
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+	}
+	
+	@ArquillianResource
+    URL deploymentUrl;
+	
+	@Test 
+	@InSequence(1)
+	public void getAllMcc_mncEntities() throws Exception {
+		request = new ClientRequest(deploymentUrl.toString() + RESOURCE_PREFIX + "/network_entities/mcc_mnc");
+		request.header("Accept", MediaType.APPLICATION_JSON);
+		// we're expecting a String back
+        ClientResponse<String> responseObj = request.get(String.class);
+        assertEquals(200, responseObj.getStatus());
+        String response = responseObj.getEntity().trim();
+        System.out.println("The response is: " + response);
+	}
+	
+	@Test
+	@InSequence(2)
+	public void testMcc_mncNotFoundById() throws Exception {
+		request = new ClientRequest(deploymentUrl.toString() + RESOURCE_PREFIX + "/network_entities/mcc_mnc/238/3");
+		request.header("Accept", MediaType.APPLICATION_JSON);
+		// we're expecting a String back
+        responseObj = request.get(String.class);
+        assertEquals(404, responseObj.getStatus());
+        String response = responseObj.getEntity().trim();
+        System.out.println("The response is: " + response);
+	}
+	
+	@Test
+	@InSequence(3)
+	public void testPostMcc_mnc() throws Exception {
+		jsonData = "{\"mcc\":238,\"mnc\":3,\"country\":\"Denmark\",\"operator\":\"MIGway A/S DK\"}";
+		request = new ClientRequest(deploymentUrl.toString() + RESOURCE_PREFIX + "/network_entities/mcc_mnc");
+		request.accept("application/json").body(MediaType.APPLICATION_JSON, jsonData);
+		responseObj = request.post(String.class);
+		assertEquals(201, responseObj.getStatus());
+		String response = responseObj.getEntity().trim();
+		System.out.println("The response is: " + response);
+		
+	}
+	
+	@Test
+	@InSequence(4)
+	public void testCreatedMcc_mncFoundById() throws Exception {
+		request = new ClientRequest(deploymentUrl.toString() + RESOURCE_PREFIX + "/network_entities/mcc_mnc/238/3");
+		request.header("Accept", MediaType.APPLICATION_JSON);
+		// we're expecting a String back
+        responseObj = request.get(String.class);
+        assertEquals(200, responseObj.getStatus());
+        String response = responseObj.getEntity().trim();
+        System.out.println("The response is: " + response);
+	}
+	
+	@Test
+	@InSequence(5)
+	public void testPutMcc_mnc() throws Exception {
+		jsonData = "{\"mcc\":238,\"mnc\":4,\"country\":\"Denmark\",\"operator\":\"MIGway A/S DK\"}";
+		request = new ClientRequest(deploymentUrl.toString() + RESOURCE_PREFIX + "/network_entities/mcc_mnc/238/3");
+		request.accept("application/json").body(MediaType.APPLICATION_JSON, jsonData);
+		responseObj = request.put(String.class);
+		assertEquals(200, responseObj.getStatus());
+		String response = responseObj.getEntity().trim();
+		System.out.println("The response is: " + response);
+		
+	}
+	
+	@Test
+	@InSequence(6)
+	public void testChangedMcc_mncFoundById() throws Exception {
+		request = new ClientRequest(deploymentUrl.toString() + RESOURCE_PREFIX + "/network_entities/mcc_mnc/238/4");
+		request.header("Accept", MediaType.APPLICATION_JSON);
+		// we're expecting a String back
+        responseObj = request.get(String.class);
+        assertEquals(200, responseObj.getStatus());
+        String response = responseObj.getEntity().trim();
+        System.out.println("The response is: " + response);
+	}
+	@Test
+	@InSequence(7)
+	public void testMcc_mncDeleteById() throws Exception {
+		request = new ClientRequest(deploymentUrl.toString() + RESOURCE_PREFIX + "/network_entities/mcc_mnc/238/4");
+		request.header("Accept", MediaType.APPLICATION_JSON);
+		// we're expecting a String back
+        responseObj = request.delete(String.class);
+        assertEquals(204, responseObj.getStatus());
+	}
+	@Test
+	@InSequence(8)
+	public void testMcc_mncNotFoundAfterDeleteById() throws Exception {
+		request = new ClientRequest(deploymentUrl.toString() + RESOURCE_PREFIX + "/network_entities/mcc_mnc/238/4");
+		request.header("Accept", MediaType.APPLICATION_JSON);
+		// we're expecting a String back
+        responseObj = request.get(String.class);
+        assertEquals(404, responseObj.getStatus());
+	}
+}
