44d87b900d2dfac4d31964ee3a2a4b9e10bd78fe
Robmeister2015 <A00226084@student.ait.ie>
2016-06-09 08:30:08 +0100

    rerterter

--- src/test/java/com/dao/test/SendValidatedInfoToDBtest.java
+++ src/test/java/com/dao/test/SendValidatedInfoToDBtest.java
@@ -12,23 +12,52 @@ import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import com.ait.db.data.CompositePrimaryKeyType;
 import com.ait.db.data.NetworkEntityDAO;
+import com.ait.db.data.NetworkEntityDAO.NetworkEntityTypeEnumFactory;
+import com.ait.db.data.NetworkEntityDAO.PrimaryKeyFactory;
+import com.ait.db.data.NetworkEntityType;
 import com.ait.db.model.Base_data;
+import com.ait.db.model.CompositePK;
+import com.ait.db.model.Event_cause;
+import com.ait.db.model.Event_causeKey;
+import com.ait.db.model.Failure_class;
+import com.ait.db.model.Mcc_mnc;
+import com.ait.db.model.Mcc_mncKey;
+import com.ait.db.model.NetworkEntity;
+import com.ait.db.model.User_equipment;
 import com.ait.reader.ReadDataSetIntoMainMemory;
+import com.errorlogger.DatabaseErrorLogger;
+import com.errorlogger.DateManipulator;
+import com.errorlogger.WriteErrorsToFile;
+import com.fileuploader.ExcelFileUploader;
+import com.validation.DataValidator;
+import com.validation.JDBCConnectionManager;
 import com.validation.SendValidatedInfoToDB;
+import com.validation.ValidationDataFromJDBC;
 
 @RunWith(Arquillian.class)
 public class SendValidatedInfoToDBtest {
 
 	@Deployment
 	public static Archive<?> createDeployment() {
-		return ShrinkWrap.create(WebArchive.class, "test.war").addPackage(Base_data.class.getPackage())
+		WebArchive archive = ShrinkWrap.create(WebArchive.class, "test.war").addPackage(Base_data.class.getPackage())
 				.addPackage(NetworkEntityDAO.class.getPackage())
 				.addPackage(ReadDataSetIntoMainMemory.class.getPackage())
 				.addPackage(SendValidatedInfoToDB.class.getPackage())
 				.addAsResource("test-persistence.xml", "META-INF/persistence.xml")
 	            .addAsWebInfResource("jbossas-ds.xml")
 	            .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+		archive.addClasses(CompositePrimaryKeyType.class, NetworkEntityDAO.class, NetworkEntityType.class);
+		archive.addClasses(PrimaryKeyFactory.class, NetworkEntityTypeEnumFactory.class);
+		archive.addClasses(User_equipment.class, Base_data.class, Event_cause.class, Event_causeKey.class,
+				Mcc_mncKey.class, Failure_class.class, CompositePK.class, NetworkEntity.class, Mcc_mnc.class);
+		archive.addClass(ReadDataSetIntoMainMemory.class);
+		archive.addClasses(DatabaseErrorLogger.class, DateManipulator.class, WriteErrorsToFile.class);
+		archive.addClass(ExcelFileUploader.class);
+		archive.addClasses(ValidationDataFromJDBC.class, JDBCConnectionManager.class, SendValidatedInfoToDB.class,
+				DataValidator.class);
+		return archive;
 	}
 	
 	
@@ -38,10 +67,7 @@ public class SendValidatedInfoToDBtest {
 		String fileName = "C:\\Users\\A00226084\\Downloads\\AIT Group Project - Sample Dataset.xls";
 		try {
 			sendValidatedInfoToDB.sendData(ReadDataSetIntoMainMemory.readFileInFromHardDrive(fileName).get(1), 1);
-		} catch (InvalidFormatException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
+		}  catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
