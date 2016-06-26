7a79463e832d6c432c7e91bb4f29d8856dd6364e
UrbanBogger <mitja.suncic@gmail.com>
2016-06-26 11:21:28 +0100

    Have added a method that calculates total failure duration for each distinct IMSI. Have added an Arquillian test that tests whether the calculations have been performed correctly.

--- src/main/java/com/ait/imsiStats/IMSIStatsProducer.java
+++ src/main/java/com/ait/imsiStats/IMSIStatsProducer.java
@@ -4,6 +4,7 @@ import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import com.ait.db.model.Base_data;
 import com.ait.db.model.NetworkEntity;
@@ -11,6 +12,7 @@ import com.ait.db.model.NetworkEntity;
 public class IMSIStatsProducer {
 	private List<? extends NetworkEntity> baseDataList;
 	private Map<BigInteger, Integer> numberOfFailures;
+	private Map<BigInteger, Integer> durationOfFailures;
 	private int failuresCounter;
 	private BigInteger imsi;
 	
@@ -18,8 +20,6 @@ public class IMSIStatsProducer {
 	public IMSIStatsProducer(List<? extends NetworkEntity> baseDataList) {
 		this.baseDataList = baseDataList;
 	}
-	
-	
 	public int getLengthOfList() {
 		return baseDataList.size();
 	}
@@ -41,7 +41,33 @@ public class IMSIStatsProducer {
 		}
 		return numberOfFailures;
 	}
-	
+	public Map<BigInteger, Integer> calculateTotalFailureTime() {
+		Map<BigInteger, Integer> numOfFailures = countTheNumberOfFailures();
+		durationOfFailures = new HashMap<>();
+		if(!numOfFailures.isEmpty()) {
+			Set<BigInteger> imsiKeys = numOfFailures.keySet();
+			int failureDuration = 0;
+			for(BigInteger imsiKey : imsiKeys) {
+				for(NetworkEntity baseData : baseDataList) {
+					if(baseData instanceof Base_data) {
+						imsi =  ((Base_data) baseData).getImsi();
+						if(imsiKey.equals(imsi)) {
+							failureDuration += ((Base_data) baseData).getDuration();
+						} else {
+							continue;
+						}
+					} else {
+						return durationOfFailures;
+					}
+				}
+				durationOfFailures.put(imsiKey, failureDuration);
+				failureDuration = 0;
+			}
+			return durationOfFailures;
+		} else {
+			return durationOfFailures;
+		}	
+	}
 	
 	
 }
