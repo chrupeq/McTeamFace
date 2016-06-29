8e28f93494d9cd97f96864546eabe89db37669c0
UrbanBogger <mitja.suncic@gmail.com>
2016-06-29 21:32:05 +0100

    Made changes to the IMSIFailures class: added a report id data member so that it's displayed in the data table on the client.

--- src/main/java/com/ait/db/model/IMSIWithValidFailureClasses.java
+++ src/main/java/com/ait/db/model/IMSIWithValidFailureClasses.java
@@ -4,16 +4,20 @@ import java.math.BigInteger;
 import java.util.Calendar;
 
 public class IMSIWithValidFailureClasses {
+	private int reportId;
 	private String dateTime;
 	private int failureClass;
 	private BigInteger imsi;
 	
-	public IMSIWithValidFailureClasses(String dateTime, int failureClass, BigInteger imsi) {
+	public IMSIWithValidFailureClasses(int reportId, String dateTime, int failureClass, BigInteger imsi) {
+		this.reportId = reportId;
 		this.dateTime = dateTime;
 		this.failureClass = failureClass;
 		this.imsi = imsi;
 	}
-	
+	public int getReportID() {
+		return reportId;
+	}
 	public String getDate_time() {
 		return dateTime;
 	}
