da37bb6450944bf1016d3f57f8757623a4c2f379
Garrett <Garrett@GarrettPC>
2016-07-07 11:39:27 +0100

    Re-commit

--- src/main/java/com/fileuploader/ExcelFileUploader.java
+++ src/main/java/com/fileuploader/ExcelFileUploader.java
@@ -88,6 +88,7 @@ public class ExcelFileUploader extends JDBCConnectionManager {
 		objectsToBePersisted[0] = baseData;
 		String date = networkEntityDAO.saveNetworkEntityArray((NetworkEntity[]) objectsToBePersisted[0]);
 		
+		//settime
 		ft.setEndTime(date);
 		fileTimerDAO.update(ft);
 	}
