0d43f6deeccc8283a38cc9ed00a77ee8181877bc
UrbanBogger <mitja.suncic@gmail.com>
2016-07-19 12:43:17 +0100

    US#11:One REST test added;US#12: Two REST tests added
    PP with Laura

--- src/test/java/com/dao/test/TopTenRestServiceTest.java
+++ src/test/java/com/dao/test/TopTenRestServiceTest.java
@@ -26,6 +26,7 @@ import com.ait.db.data.TopTenDAO;
 import com.ait.db.model.Base_data;
 import com.ait.db.rest.IMSIRestService;
 import com.ait.db.rest.JaxRsActivator;
+import com.ait.imsiStats.IMSIStats;
 
 @RunWith(Arquillian.class)
 @RunAsClient
@@ -43,7 +44,7 @@ public class TopTenRestServiceTest {
 		return ShrinkWrap.create(WebArchive.class, "test.war")
 				.addPackage(Base_data.class.getPackage())
         		.addPackage(TopTenDAO.class.getPackage())
-        		.addClasses(IMSIRestService.class, JaxRsActivator.class)
+        		.addClasses(IMSIStats.class, IMSIRestService.class, JaxRsActivator.class)
         		.addAsResource("test-persistence.xml", "META-INF/persistence.xml")
                 .addAsResource("import.sql")
                 .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
@@ -60,6 +61,33 @@ public class TopTenRestServiceTest {
 		// we're expecting a String back
         ClientResponse<String> responseObj = request.get(String.class);
         assertEquals(200, responseObj.getStatus());
+	}
+	@Test
+	public void getTopTenMarketOperatorCellIdCombinationsWithFailuresShouldReturnA404() throws Exception{
+		request = new ClientRequest(deploymentUrl.toString() + RESOURCE_PREFIX + "/imsi/"
+				+ "top10_market_operator_cellIdCombinations?dateOne=11/01/1970+17:00&dateTwo=11/01/1971+17:30");
+		request.header("Accept", MediaType.APPLICATION_JSON);
+		// we're expecting a String back
+        ClientResponse<String> responseObj = request.get(String.class);
+        assertEquals(404, responseObj.getStatus());
+	}
+	@Test
+	public void getTopTenIMSIShouldReturn200() throws Exception{
+		request = new ClientRequest(deploymentUrl.toString() + RESOURCE_PREFIX + "/imsi/"
+				+ "top10_IMSI?dateOne=11/01/2013+17:00&dateTwo=11/01/2013+17:30");
+		request.header("Accept", MediaType.APPLICATION_JSON);
+		// we're expecting a String back
+        ClientResponse<String> responseObj = request.get(String.class);
+        assertEquals(200, responseObj.getStatus());
+	}
+	@Test
+	public void getTopTenIMSIShouldReturn404() throws Exception{
+		request = new ClientRequest(deploymentUrl.toString() + RESOURCE_PREFIX + "/imsi/"
+				+ "top10_IMSI?dateOne=11/01/1970+17:00&dateTwo=11/01/1971+17:30");
+		request.header("Accept", MediaType.APPLICATION_JSON);
+		// we're expecting a String back
+        ClientResponse<String> responseObj = request.get(String.class);
+        assertEquals(404, responseObj.getStatus());
 	}
 
 }
