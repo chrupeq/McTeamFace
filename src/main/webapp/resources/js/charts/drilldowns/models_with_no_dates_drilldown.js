2998ebacd636b6269127d58dac557f2c5fe2087b
A00226084 <A00226084@X205-09.studentnet.ait.ie>
2016-07-20 20:09:14 +0100

    First drilldown complete and added. Needs style.

--- src/main/webapp/resources/js/charts/drilldowns/models_with_no_dates_drilldown.js
+++ src/main/webapp/resources/js/charts/drilldowns/models_with_no_dates_drilldown.js
@@ -1,5 +1,5 @@
-var drilldown1Url = 'http://localhost:8080/GroupProject2016/rest/drilldown/drill_down?';
-
+var drilldown1Url = 'http://localhost:8080/GroupProject2016/rest/drill_down/failure_event_desc?';
+var dataArray = [];
 var getModelInfoForDrilldown = function(eventId, causeCode, tacNumber){
 	$('#queryprogress').css('width', '0%');
  	$('#queryprogress').text('0%');
@@ -20,13 +20,93 @@ var getModelInfoForDrilldown = function(eventId, causeCode, tacNumber){
 	        }
 	    },
 		success: function(data){
-			//loadImsiEventIdCauseCodeTable(data);
-			alert("hi");
+			modelDrillDownTable(data);
+			$('#canvasdiv2').removeClass('hiddenbycostanza');
+			$('#canvasdiv2').removeClass('animated fadeOutUp');
+			$('#canvasdiv2').addClass('animated fadeInDown');
 			$('#queryprogressouter').mousemove(function(){
 				$('#queryprogressouter').removeClass('animated fadeInDown');
 				$('#queryprogressouter').addClass('animated fadeOutUp');
 			});
 		},
-		error: $('#tableDiv').html('<h3 id="noDataMessage">no data to display for this query<h3>')
 	});
+}
+
+var modelDrillDownTable = function(data){
+dataArray = data;
+	var table = '<table class="display table table-striped table-hover table-condensed animated fadeInDown" id="drilldownTable">'
+	+ '<thead>'
+	+ '<tr>'
+	+ '<th align="left">Date/Time</th>'
+	+ '<th align="left" class="col-sm-2">Report ID</th>'
+	+ '<th align="left" class="col-sm-2">Event Cause Description</th>'
+	+ '<th align="left" class="col-sm-2">Failure Description</th>'
+	+ '<th align="left" class="col-sm-2">Country</th>'
+	+ '<th align="left" class="col-sm-2">Operator</th>'
+	+ '</tr>'
+	+ '</thead>'
+	+ '</table>';
+	
+	$('#canvasdiv2').html(table);
+
+	 drilldownTable = $('#drilldownTable').DataTable( {
+		 
+	        data: data,
+	        
+	        rowId: "report_id",
+	        
+	        "pageLength": 5,
+	        
+	        "columnDefs": [
+	                       { "width": "20%", "targets": 0,
+	                    	   "targets": [1],
+	                           "visible": false,
+	                           "searchable": false,
+	                    	   }
+	                       
+	                     ],
+	        columns: [
+
+	            { data: "date_time",
+	            	render : function(data) {
+	            		
+	            		var date = new Date(data);
+						return date.getUTCDate() + '/' + (date.getUTCMonth() + 1)+ '/' + date.getFullYear() + ' ' + date.getHours() + ':' + date.getMinutes();
+					}
+	            },
+	            
+	            { data: "report_id" },
+	            
+	            { data: "event_cause.description" },
+	            
+	            { data: "failure_class.description" },
+	            
+	            { data: "mcc_mnc.country" },
+	            
+	            { data: "mcc_mnc.operator" },
+	            
+	        ],
+	        	        
+})
+
+$('#drilldownTable tbody').on( 'click', 'tr', function () {
+	var reportId = drilldownTable.row(this).data().report_id;
+	showMoreInfoModal(reportId);
+	});
+}
+
+var showMoreInfoModal = function(reportId){
+	var stuffToGoIntoModalBody = "";
+	for(var i = 0; i < dataArray.length; i ++){
+		if(dataArray[i].report_id == reportId){
+			$('#drilldownmodaltitle').html('<h2>More information for ' + dataArray[i].user_equipment.manufacturer + ' ' + dataArray[i].user_equipment.marketing_name);
+			stuffToGoIntoModalBody += '<h4>Handset Manufacturer: </h4><p>' + dataArray[i].user_equipment.manufacturer + '</p>'
+									+ '<h4>Handset Model: </h4><p>' + dataArray[i].user_equipment.marketing_name + '</p>'
+									+ '<h4>Country: </h4><p>' + dataArray[i].mcc_mnc.country + '</p>'
+									+ '<h4>Handset Model: </h4><p>' + dataArray[i].mcc_mnc.operator + '</p>';
+									
+		}
+	}
+	$('#drilldownmodalcenter').html(stuffToGoIntoModalBody);
+	$('#drilldownmodal').modal('show');
 }
