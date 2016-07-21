d12676f5e2c21417a48ae19836b3fdebb36b944c
Garrett <Garrett@GarrettPC>
2016-07-21 13:47:58 +0100

    Drilldown - world map

---
+++ src/main/webapp/resources/js/worldMap.js
@@ -0,0 +1,20 @@
+window.map = AmCharts.makeChart( "chartdiv", {
+
+  "type": "map",
+  "theme": "none",
+  "projection":"miller",
+
+  "dataProvider": {
+    "map": "worldLow",
+    "getAreasFromMap": true
+  },
+  "areasSettings": {
+    "autoZoom": true,
+    "selectedColor": "#CC0000"
+  },
+  "smallMap": {},
+  "export": {
+    "enabled": true,
+    "position": "bottom-right"
+  }
+} );
